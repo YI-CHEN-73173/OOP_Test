ac83498d4aa5625961b2e0fbe08bee11aca2fe44
YI_CHEN_73173 <jessiechen.mg11@nycu.edu.tw>
2025-04-21 19:41:01 +0800

    Lab07_ver1

--- src/Cylinder.cpp
+++ src/Cylinder.cpp
@@ -2,32 +2,48 @@
 # define CYLINDER_CPP
 
 # include "Cylinder.h"
+# define _USE_MATH_DEFINES
+# include <cmath>
 
 double Cylinder :: SurfaceArea(){
+    double pi=M_PI;
+    double ans=0;
     
-    return ;
+    ans=r*r*pi*2+r*2*pi*h;
+
+    return ans;
 }
 
 double Cylinder :: Volume(){
+    double pi=M_PI;
+    double ans=0;
 
-    return ;
+    ans=r*r*pi*h;
+
+    return ans;
 }
 
 double Cylinder :: Circumference(){
+	double pi=M_PI;
+	double ans=0;
 
-    return ;
+	ans=r*2*pi*2;
+
+    return ans;
 }
 
 istream & operator>>(istream & in, Cylinder & cldr)
 {
-
-    return ;
+    in>>cldr.r>>cldr.h;
+    return in;
 }
 
 ostream & operator<<(ostream & out, Cylinder & cldr)
 {
-    
-    return ;
+    out<<"Circumference: "<<Circumference()<<endl;
+    out<<"SurfaceArea: "<<SurfaceArea()<<endl;
+    out<<"Volume: "<<Volume()<<endl;
+    return out;
 }
 
 # endif
